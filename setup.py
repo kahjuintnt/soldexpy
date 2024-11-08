05d19cfd6c73e071775b7d6e40a818bfd797d160
kahjuintnt <80038308+kahjuintnt@users.noreply.github.com>
2024-11-08 08:40:24 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import setuptools
 
 setuptools.setup(
     name="soldexpy",
-    version="0.0.1",
+    version="0.0.2",
     author="leq6c",
     packages=setuptools.find_packages(),
     install_requires=[
         "base58",
         "solders",
-        "solana==0.31.0",
+        "solana==0.34.3",
     ],
 )
 """
